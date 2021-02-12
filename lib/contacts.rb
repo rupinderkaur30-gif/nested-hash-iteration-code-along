a9a868fbcfce29390013ccf47ca2d10594905cd6
rupinderkaur30-gif <rupi063094@gmail.com>
2021-02-12 16:46:37 -0500

    commit tree 2f0fa87d0b4d158cce4f722720db27ac5be19484

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,17 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+    if person =  "Freddy Mercury"
+    contact_details_hash.each do |attributes,data|
+      if attributes ==  :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
 end
+end 
+
+
+
+
